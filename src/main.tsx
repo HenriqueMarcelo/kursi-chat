c0111bfaa55380f2bafc5704ce125fce183213e5
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-02-01 16:44:23 -0300

    Installing Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Chat from './pages/Chat'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Chat />
   </React.StrictMode>,
 )
